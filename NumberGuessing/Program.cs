1c333890072d12de9f463dfc26ca9a00b6a30e62
Burak <Burak@DESKTOP-97V21EN>
2024-12-11 02:59:17 +0300

    Add project files.

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessing
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrectGuess = false;
+            Random random = new Random();
+
+            int randomSayı = random.Next(1, 11);
+            
+            Console.WriteLine("Sayı Falcısına Hoşgeldiniz! :)");
+            Console.WriteLine("Aklımdan 1-10 arasında bir sayı tutacağım");
+            Console.WriteLine("Eğer doğru bilirsen, kazanırsın");
+
+
+
+            while (!isCorrectGuess)
+            {
+                Console.WriteLine("Lütfen tahmininizi giriniz.");
+                int tahmin = Convert.ToInt32(Console.ReadLine());
+
+                if (tahmin > randomSayı)
+                {
+                    Console.WriteLine("Tahmininiz yüksek, daha düşük sayılar deneyin!");
+                }
+                else if (tahmin < randomSayı) 
+                {
+                    Console.WriteLine("Tahmininiz düşük, daha yüksek sayılar deneyin!");
+                }
+                else
+                {
+                    Console.WriteLine("Doğru tahmin!");
+                    isCorrectGuess = true;
+                }
+            }
+
+            Console.WriteLine("Oyunu kazandınız, tebrikler!");
+
+            
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
